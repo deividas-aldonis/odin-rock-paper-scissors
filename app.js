cd61c6c54efa3c12fea9279b455ab670d300d62f
Deividas <deividas.aldonis.dev@gmail.com>
2022-11-19 21:11:03 +0200

    Refactor checkWinner() function logic

--- app.js
+++ app.js
@@ -3,24 +3,41 @@ const rock = document.querySelector('.rock');
 const scissors = document.querySelector('.scissors');
 const result = document.querySelector('.result');
 
+let playerScore = 0;
+let enemyScore = 0;
+
 const getComputerChoice = () => ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
 
 const checkWinner = (playerSelection, enemySelection) => {
     const playerAndEnemySelection = playerSelection + enemySelection;
 
-    const combinations = {
-        paperscissors: 'You Lose! Scissors beat paper.',
-        paperrock: 'You Win! Paper beats rock.',
-        rockscissors: 'You Win! Rock beats scissors.',
-        rockpaper: 'You Lose! Paper beats rock.',
-        scissorspaper: 'You Win! Scissors beats paper.',
-        scissorsrock: 'You Lose! Rock beats scissors.',
-        paperpaper: "It's a tie!",
-        rockrock: "It's a tie!",
-        scissorsscissors: "It's a tie!"
-    };
+    switch (playerAndEnemySelection) {
+        case 'paperscissors':
+            enemyScore += 1;
+            break;
+        case 'rockpaper':
+            enemyScore += 1;
+            break;
+        case 'scissorsrock':
+            enemyScore += 1;
+            break;
+        case 'paperrock':
+            playerScore += 1;
+            break;
+        case 'rockscissors':
+            playerScore += 1;
+            break;
+        case 'scissorspaper':
+            playerScore += 1;
+            break;
+        default:
+    }
 
-    result.textContent = combinations[playerAndEnemySelection];
+    if (playerScore === 5) result.textContent = 'Player Wins!';
+    if (enemyScore === 5) result.textContent = 'Enemy Wins!';
+
+    document.querySelector('.player').textContent = playerScore;
+    document.querySelector('.enemy').textContent = enemyScore;
 };
 
 const playRound = (playerSelection) => {

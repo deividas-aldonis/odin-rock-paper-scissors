4ab4fb3a3160dcc8973641ea556e397df5e04444
Deividas <deividas.aldonis.dev@gmail.com>
2022-11-20 01:32:54 +0200

    Add modal to display the winner

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const paper = document.querySelector('.paper');
 const rock = document.querySelector('.rock');
 const scissors = document.querySelector('.scissors');
+
+const windowOverlay = document.querySelector('.window-overlay');
+const modal = document.querySelector('.modal');
+const closeModalBtn = document.querySelector('.close');
+const playAgainBtn = document.querySelector('.play-again');
 const result = document.querySelector('.result');
 
 let playerScore = 0;
 let enemyScore = 0;
+
+const openModal = () => {
+    windowOverlay.classList.add('show');
+    modal.classList.add('show');
+};
+
+const closeModal = () => {
+    windowOverlay.classList.remove('show');
+    modal.classList.remove('show');
+};
+
+const playAgain = () => {
+    playerScore = 0;
+    enemyScore = 0;
+    document.querySelector('.player').textContent = playerScore;
+    document.querySelector('.enemy').textContent = enemyScore;
+    closeModal();
+};
 
 const getComputerChoice = () => ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
 
@@ -33,14 +56,25 @@ const checkWinner = (playerSelection, enemySelection) => {
         default:
     }
 
-    if (playerScore === 5) result.textContent = 'Player Wins!';
-    if (enemyScore === 5) result.textContent = 'Enemy Wins!';
+    if (playerScore > 4) {
+        result.textContent = 'Good job! You win!';
+        openModal();
+    }
+    if (enemyScore > 4) {
+        result.textContent = 'Computer wins! Better luck next time!';
+        openModal();
+    }
 
     document.querySelector('.player').textContent = playerScore;
     document.querySelector('.enemy').textContent = enemyScore;
 };
 
 const playRound = (playerSelection) => {
+    if (playerScore > 4 || enemyScore > 4) {
+        result.textContent = 'The game is over! If you want to play more press "play again"';
+        openModal();
+        return;
+    }
     const computerSelection = getComputerChoice();
 
     checkWinner(playerSelection, computerSelection);
@@ -49,3 +83,5 @@ const playRound = (playerSelection) => {
 paper.addEventListener('click', () => playRound('paper'));
 rock.addEventListener('click', () => playRound('rock'));
 scissors.addEventListener('click', () => playRound('scissors'));
+closeModalBtn.addEventListener('click', closeModal);
+playAgainBtn.addEventListener('click', playAgain);
